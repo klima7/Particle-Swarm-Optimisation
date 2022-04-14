02c090d6fbb0f811d806334352a83b60d4e9b3c9
klima7 <ukasz.klimkiewicz@gmail.com>
2022-04-14 15:03:09 +0200

    Basic template created

---
+++ swarm.py
@@ -0,0 +1,8 @@
+class PSOSolver:
+
+    def __init__(self, w_mean=1, c1=1, c2=1):
+        self.c1 = c2
+        self.c2 = c2
+
+    def solve(self, fun, domain):
+        return 0
---
+++ test.py
@@ -0,0 +1,17 @@
+from swarm import PSOSolver
+
+
+def fun(vec):
+    x, y = vec
+    term1 = (1.5 - x - x * y) ** 2
+    term2 = (2.25 - x + x * (y ** 2)) ** 2
+    term3 = (2.625 - x + x * (y ** 3)) ** 2
+    result = term1 + term2 + term3
+    return result
+
+
+if __name__ == '__main__':
+    solver = PSOSolver()
+    domain = [[-4.5, 4.5], [-4.5, 4.5]]
+    solution = solver.solve(fun, domain)
+    print(f'Solution: {solution}')
