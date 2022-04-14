48940f761e9eadea9c00bb127239f0efd5ab6fea
klima7 <ukasz.klimkiewicz@gmail.com>
2022-04-14 23:37:48 +0200

    Visualizing function levels

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from swarm import PSOSolver
 
 
-def fun(vec):
+def function(vec):
     x, y = vec
     term1 = (1.5 - x - x * y) ** 2
     term2 = (2.25 - x + x * (y ** 2)) ** 2
@@ -14,8 +14,14 @@ def fun(vec):
 
 def test(solver):
 
+    # function and domain
+    fun = function
+    domain = np.array([[-4.5, 4.5], [-4.5, 4.5]])
+
+    # plot visualization
     fig, ax = plt.subplots()
-    ax.axis([-4.5, 4.5, -4.5, 4.5])
+    ax.axis(domain.flatten())
+    plot_background(ax, fun, domain)
     sc = ax.scatter([], [], marker='o')
     ax.set_title('Particles')
     ax.set_xlabel('x')
@@ -28,6 +34,7 @@ def test(solver):
         fontsize=12,
     )
 
+    # update visualization
     def update_plot(best_arg, best_value, pos):
         x = [p[0] for p in pos]
         y = [p[1] for p in pos]
@@ -37,11 +44,18 @@ def test(solver):
         plt.draw()
         plt.pause(0.05)
 
+    # solve
     np.random.seed(42)
-    domain = [[-4.5, 4.5], [-4.5, 4.5]]
     arg, val = solver.solve(fun, domain, callback=update_plot)
-
     print(f'Args: {arg}; Value: {val}')
+
+
+def plot_background(ax, fun, domain):
+    resolution = 100
+    space = np.meshgrid(np.linspace(domain[0, 0], domain[0, 1], resolution), np.linspace(domain[1, 0], domain[1, 1], resolution))
+    space_stacked = np.dstack([*space]).reshape(-1, 2)
+    values = np.array([fun(arg) for arg in space_stacked]).reshape(resolution, resolution)
+    ax.contourf(space[0], space[1], values, levels=20, alpha=0.2)
 
 
 if __name__ == '__main__':
