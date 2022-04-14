f535a0676ebeabf40392455177486a5f96c8a384
klima7 <ukasz.klimkiewicz@gmail.com>
2022-04-14 17:12:45 +0200

    Updating local and global minimums

--- swarm.py
+++ swarm.py
@@ -12,8 +12,18 @@ class PSOSolver:
 
     def solve(self, fun, domain):
         domain = np.array(domain)
-        x = self._random_positions(domain)
-        v = self._random_velocities(domain.ndim)
+        n_dim = domain.shape[0]
+
+        pos = self._random_positions(domain)
+        vel = self._random_velocities(n_dim)
+        lv = np.repeat(np.inf, self.n_particles)
+        lp = np.zeros((self.n_particles, n_dim))
+        gv = np.inf
+        gp = np.zeros((n_dim,))
+
+        for i in range(100):
+            lv, lp, gv, gp = self._get_minimums(lv, lp, gv, gp, fun, pos)
+
         return 0
 
     def _random_positions(self, domain):
@@ -32,3 +42,26 @@ class PSOSolver:
         scale_factors_transformed = np.repeat(np.expand_dims(scale_factors, axis=1), 2, axis=1)
         scaled_directions = directions * scale_factors_transformed
         return scaled_directions
+
+    def _get_minimums(self, lv, lp, gv, gp, fun, pos):
+        # calling function
+        values = self._get_function_values(fun, pos)
+
+        # local minimums
+        where_better = values < lv
+        lv[where_better] = values[where_better]
+        lp[where_better] = pos[where_better]
+
+        # global minimum
+        min_index = np.argmin(values)
+        min_value = values[min_index]
+        if min_value < gv:
+            gv = min_value
+            gp = pos[min_index]
+
+        return lv, lp, gv, gp
+
+    @staticmethod
+    def _get_function_values(fun, pos):
+        return np.array([fun(args) for args in pos])
+
